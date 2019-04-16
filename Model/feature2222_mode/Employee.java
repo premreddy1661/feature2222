cc5b4bad78442501eba9c4e4d8d1f07ddea5bc61
fhgkyfhui <premreddy1661@gmail.com>
2019-04-15 23:08:05 -0500

    added

---
+++ Model/feature2222_mode/Employee.java
@@ -0,0 +1,18 @@
+package feature2222_mode;
+
+public class Employee {
+private String employeName;
+private int employeeNumber;
+public String getEmployeName() {
+	return employeName;
+}
+public void setEmployeName(String employeName) {
+	this.employeName = employeName;
+}
+public int getEmployeeNumber() {
+	return employeeNumber;
+}
+public void setEmployeeNumber(int employeeNumber) {
+	this.employeeNumber = employeeNumber;
+}
+}
